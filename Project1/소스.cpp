99ae386e17a9c9913ddd60afa72c0b1f94af5e71
user <user@PC10>
2021-09-01 16:30:45 +0900

    초기 커밋.

---
+++ Project1/소스.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include<cstdlib>
+#include<ctime>
+
+using namespace std;
+//Game클래스 선언
+
+class Game {
+private:
+	double energy = 100;
+	static int life;
+
+public:
+	Game();
+	double Damage();
+	static void Die();
+};
+int Game::life = 10;
+
+void Game::Die() {
+	life -= rand() % 2 + 1;
+	
+	if (life <= 0) {
+		cout << "생명이 감소하여 " << life << "이 되었습니다." << endl;
+		cout << "**생명이 부족하여 사망했습니다." << endl;
+	}
+	
+}
+double Game::Damage() {
+	double damaged = 0;
+
+	if (energy > 0) {
+		damaged += rand() % 100;
+		energy -= damaged;
+		cout << "에너지가 " << damaged << " 감소하여 " << energy << " 이 되었습니다.\n";
+		if (energy < 0) {
+			life -= rand() % 2 + 1;
+			energy = 100;
+			cout << "생명이 감소하여 " << life << "이 되었습니다." << endl << endl;
+
+		}
+	}
+	return energy;
+}
+//Game클래스 멤버 함수의 정의
+Game::Game()
+{
+	life = 10;
+
+	cout << "에너지가 " << energy << " 이며, 생명이 " << life << "인 캐릭터가 만들어졌습니다.\n";
+	
+	while (life >= 0) {
+
+		Damage();
+	}
+
+}
+
+int main()
+{
+	srand((unsigned int)time(NULL)); //seed값으로 현재시간 부여
+	Game c1;
+	Game::Die();
+	Game c2;
+	c2.Damage();
+	c2.Damage();
+	c2.Damage();
+	c2.Die();
+	return 0;
+}
